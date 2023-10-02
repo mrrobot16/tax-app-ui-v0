768b4aab41e744dc3be3fda42e0a4e94970146c1
Hector G <hmgarcia06@gmail.com>
2023-10-02 17:29:02 -0400

    Moved AppRouter to another folder

---
+++ src/router/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
+import { Chat } from 'containers';
+
+export const routes = [
+    {
+        path: '/',
+        element: <Chat />,
+    },
+    {
+      path: '/chat',
+      element: <Chat />,
+  },
+];
+
+
+const router = createBrowserRouter(routes);
+
+function Router() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default Router;
