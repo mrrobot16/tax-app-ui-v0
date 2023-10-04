5033b960fbec8c958df63c57e7dae5aa4197568a
Hector G <hmgarcia06@gmail.com>
2023-10-04 17:21:49 -0400

    Moved reusable components to common folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,13 @@
 import Layout from 'components/common/Layout';
 import Navbar from 'components/common/Navbar';
-import { MessagesList } from 'components/messages/MessagesList';
+import MessagesList from 'components/messages/MessagesList';
+import SendMessagesForm from 'components/messages/SendMessagesForm';
+import LoadingDots from 'components/common/LoadingDots';
 
 export {
     Layout,
     Navbar,
     MessagesList,
+    SendMessagesForm,
+    LoadingDots,
 };
