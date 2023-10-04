20db49230883e0c7d54656d0aa59223a25f4a7fe
Hector G <hmgarcia06@gmail.com>
2023-10-04 00:07:24 -0400

    Refactored MessagesList

--- src/components/messages/MessagesList.tsx
+++ src/components/messages/MessagesList.tsx
@@ -1,21 +1,25 @@
-import { ChatContainerStyling } from 'containers/Chat/styles';
+import { MessagesComponentsStyling } from 'components/messages/styles';
 import { UserIcon, BotIcon } from 'assets/icons';
 import { MessagesListProps, Message } from 'types';
 
-const { classNames, styles } = ChatContainerStyling;
+const { styles } = MessagesComponentsStyling;
 
 export function MessagesList({messages, callback, loading}: MessagesListProps) {
     return (
-      <div className="MessageList" style={styles.messageList}>
+      <div data-id="MessageList" style={{...styles.messageList, overflowY: 'scroll'}}>
           {
             messages.map((message: Message, index: number) => {
               const isLoading = loading && index === messages.length - 1;
-              const messageLoadingStyle = isLoading ? {...styles.userMessageLoading, ...styles.messageItem}
-              : {...styles.userMessage, ...styles.messageItem};
-              const messageStyle = message.type === 'api' ? {...styles.apiMessage, ...styles.messageItem} : messageLoadingStyle;
+              const isApiMessage = message.type === 'api';
+
+              const userMessageStyle = {...styles.messageItem, ...styles.userMessage};
+              const apiMessageStyle = {...styles.messageItem, ...styles.apiMessage};
+              const messageLoadingStyle = {...styles.messageItem, ...styles.userMessageLoading};
+
+              const messageStyle = isApiMessage ? apiMessageStyle : isLoading ? messageLoadingStyle : userMessageStyle;
 
               return (
-                  <div key={`chatMessage-${index}`} id={`chatMessage-${message.type}-${index}`} style={messageStyle}>
+                  <div key={`chatMessage-${index}`} data-id={`chatMessage-${message.type}-${index}`} style={messageStyle}>
                     {
                       message.type === 'user' ? (
                         <UserIcon index={index} height={30} width={30} styles={styles.icons} />
---
+++ src/components/messages/styles.ts
@@ -0,0 +1,38 @@
+const styles = {
+    messageList: {
+      width: '100%',
+      height: '100%',
+      overflowY: 'scroll',
+      borderRadius: '0.5rem',
+    },
+    messageItem: {
+      display: 'flex',
+      padding: '1.5rem',
+    },
+    apiMessage: {
+      background: '#f9fafb',
+      color: '#000',
+      borderRadius: '0.5rem',
+      animation: 'fadein 0.5s',
+    },
+    userMessage: {
+      // display: 'flex',
+      // padding: '1.5rem',
+    },
+    userMessageLoading: {
+      // display: 'flex',
+      // padding: '1.5rem',
+    },
+    messageInfo: {
+      lineHeight: 1.75,
+    },
+    icons: {
+      height: '100%',
+      borderRadius: '0.1rem',
+      marginRight: '1rem',
+    },
+  };
+
+export const MessagesComponentsStyling = {
+    styles,
+};
