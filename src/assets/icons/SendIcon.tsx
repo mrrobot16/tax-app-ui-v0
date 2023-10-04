f2785ad84a622d7a3fed1399d3adbc00c9fb1fde
Hector G <hmgarcia06@gmail.com>
2023-10-04 17:20:05 -0400

    Added SendIcon to assets

---
+++ src/assets/icons/SendIcon.tsx
@@ -0,0 +1,19 @@
+const styles = {
+    transform: 'rotate(90deg)',
+    width: '1.2em',
+    height: '1.2em',
+    fill: 'currentColor',
+};
+
+export function SendIcon() {
+    return (
+        <svg
+            viewBox="0 0 20 20"
+            style={styles}
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+        </svg>
+    );
+}
+export default SendIcon;
--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,9 @@
 import BotIcon from 'assets/icons/BotIcon';
 import UserIcon from 'assets/icons/UserIcon';
+import SendIcon from 'assets/icons/SendIcon';
 
 export {
     BotIcon,
     UserIcon,
+    SendIcon,
 };
