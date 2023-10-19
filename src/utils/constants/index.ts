f450c71301d298815645e5aa9c25883c1c1e6d8e
Hector G <hmgarcia06@gmail.com>
2023-10-18 20:59:29 -0400

    Change initial message from assistant

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -2,7 +2,7 @@ export const USER_ID = 'cefece6f-3a77-493c-b';
 export const CONVERSATION_ID = '7ba4cca9-4115-4a5d-a';
 export const MESSAGES_LIST = [
     {
-        content: 'Hi, how I can help you with your IRS questions?',
+        content: 'Hi, how I can help you with your tax questions?',
         role: 'assistant',
     },
 ];
