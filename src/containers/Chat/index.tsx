333ab799c0d6fd42e32ab2983ef0a35d795e1e59
Hector G <hmgarcia06@gmail.com>
2023-10-19 00:56:47 -0400

    Removed localstorage logic, refactored setMessageList setter

--- src/containers/Chat/index.tsx
+++ src/containers/Chat/index.tsx
@@ -6,56 +6,25 @@ import { MessagesList, SendMessagesForm, ErrorMessage } from 'components';
 import { MESSAGES_LIST, USER_ID, CONVERSATION_ID, ASSISTANT_LOADING_MESSAGE } from 'utils/constants';
 import { Message } from 'types';
 import { newConversationWithOpenai, newConversationMessage } from 'services';
-import {
-  setConversationIdLocalStorage,
-  getConversationIdLocalStorage,
-  getUserIdLocalStorage,
-  setUserIdLocalStorage,
-} from 'utils/storage';
 
 const { classNames, styles } = ChatContainerStyling;
 
 export function Chat() {
   const [messageList, setMessageList] = useState<Message[]>(MESSAGES_LIST);
-  const [userId, setUserId] = useState<string | null>(null);
-  const [conversationId, setConversationId] = useState<string | null>(null);
+  const [userId, setUserId] = useState<string | null>(USER_ID);
+  const [conversationId, setConversationId] = useState<string | null>(CONVERSATION_ID);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
-  // TODO: Need fix this linting issue.
-  /* eslint-disable react-hooks/exhaustive-deps */
-  const checkUser = () => {
-    // const user_id = getUserIdLocalStorage();
+  const setUpdatedMessage = (message: Message) => {
+    setMessageList((prevMessageList: Message[]) => {
+      const updatedMessages = [...prevMessageList];
 
-    // if(!user_id) {
-    //   // NOTE: For now only one user will be created which is hardcoded.
-    //   setUserIdLocalStorage(USER_ID);
-      setUserId(USER_ID);
-    // }
+      updatedMessages[updatedMessages.length - 1] = message;
 
-    // setUserId(user_id);
+      return updatedMessages;
+    });
   };
-
-  const checkConversations = () => {
-    // const conversation_id = getConversationIdLocalStorage();
-    // NOTE: For now only one conversation will be created which is hardcoded.
-
-    // if(!conversation_id) {
-    //   setConversationIdLocalStorage(CONVERSATION_ID);
-      setConversationId(CONVERSATION_ID);
-
-  //     return conversationId;
-  //   }
-
-  //   setConversationId(conversation_id);
-  };
-
-  const componentDidMount = () => {
-    checkUser();
-    checkConversations();
-  };
-
-  useEffect(componentDidMount, [checkUser, checkConversations]);
 
   const createConversationMessage = async (message: Message) => {
     setLoading(true);
@@ -65,11 +34,13 @@ export function Chat() {
       role: message.role,
     };
 
-    setMessageList((prevMessageList) => [
+    const setNewMessage = (prevMessageList: Message[]) => [
       ...prevMessageList,
       newMessage,
       ASSISTANT_LOADING_MESSAGE,
-    ]);
+    ];
+
+    setMessageList(setNewMessage);
 
     if(!conversationId) {
       const response = await newConversationWithOpenai(userId, newMessage);
@@ -80,21 +51,8 @@ export function Chat() {
         role,
       };
 
-      setConversationIdLocalStorage(conversation_id);
       setConversationId(conversation_id);
-
-      // setMessageList((prevMessageList) => [
-      //   ...prevMessageList,
-      //   assistantMessage,
-      // ]);
-      setMessageList((prevMessageList) => {
-        const updatedMessages = [...prevMessageList];
-
-        updatedMessages[updatedMessages.length - 1] = assistantMessage;
-
-        return updatedMessages;
-      });
-
+      setUpdatedMessage(assistantMessage);
       setLoading(false);
     }
 
@@ -107,13 +65,8 @@ export function Chat() {
         role,
       };
 
-      setMessageList((prevMessageList) => {
-        const updatedMessages = [...prevMessageList];
+      setUpdatedMessage(assistantMessage);
 
-        updatedMessages[updatedMessages.length - 1] = assistantMessage;
-
-        return updatedMessages;
-      });
       setLoading(false);
     }
   };
@@ -123,7 +76,7 @@ export function Chat() {
       <h1 className={classNames.title}>Chat with Tax Copilot</h1>
       <main style={styles.main}>
         <div className="MessageListContainer" style={styles.messageListContainer}>
-            <MessagesList messages={messageList} loading={loading} assistantLoadingMessage={ASSISTANT_LOADING_MESSAGE}/>
+            <MessagesList messages={messageList} loading={loading} />
         </div>
         <div className="SendMessageContainer" style={styles.sendMessageContainer as CSSProperties}>
             <SendMessagesForm loading={loading} sendMessage={createConversationMessage} />
