68b053dfbdda8c836848f1f5e42033324288699a
Hector G <hmgarcia06@gmail.com>
2023-10-26 17:35:40 -0400

    Changed typing for error message component

--- src/components/messages/ErrorMessage.tsx
+++ src/components/messages/ErrorMessage.tsx
@@ -1,7 +1,9 @@
-export default function ErrorMessage({ error }: { error?: string | null }) {
+import { AxiosError } from 'axios';
+
+export default function ErrorMessage({ error }: { error?: string | null | AxiosError}) {
   return (
     <div className="border border-red-400 rounded-md p-4">
-    <p className="text-red-500">{error}</p>
+    <p className="text-red-500">{error as string}</p>
     </div>
   );
 }
