be7cf97509c4bb442b2567b3f933a753cb992d56
Hector G <hmgarcia06@gmail.com>
2023-10-26 18:40:39 -0400

    Added width to error message component

--- src/components/messages/ErrorMessage.tsx
+++ src/components/messages/ErrorMessage.tsx
@@ -1,9 +1,15 @@
 import { AxiosError } from 'axios';
 
+const style = {
+  main: {
+    width: '75vw',
+  },
+};
+
 export default function ErrorMessage({ error }: { error?: string | null | AxiosError}) {
   return (
-    <div className="border border-red-400 rounded-md p-4">
-    <p className="text-red-500">{error as string}</p>
+    <div className="border border-red-400 rounded-md p-4" style={style.main}>
+      <p className="text-red-500">{error as string}</p>
     </div>
   );
 }
