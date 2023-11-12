fa3a8c4f011302b04e913e987bd4590275e5439f
Hector G <hmgarcia06@gmail.com>
2023-11-12 16:51:06 -0500

    Added analytics config for production

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,10 +1,23 @@
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 
+import { APP_ENV } from 'utils/constants';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
+const firebaseConfigProd = {
+  apiKey: 'AIzaSyAg-VpiII4IwmBH0jjiq8yC3p-DSlXyTKE',
+  authDomain: 'tax-app-prod-35a06.firebaseapp.com',
+  projectId: 'tax-app-prod-35a06',
+  storageBucket: 'tax-app-prod-35a06.appspot.com',
+  messagingSenderId: '64026493068',
+  appId: '1:64026493068:web:4bb5293a13b1c65d04fba9',
+  measurementId: 'G-0BZT2XP96Y',
+};
+
+
+const firebaseConfigDev = {
   apiKey: 'AIzaSyA93YAYtmidH6IvkyCVTGHCDKkFfyo_TEA',
   authDomain: 'irscopilot-dev.firebaseapp.com',
   databaseURL: 'https://irscopilot-dev-default-rtdb.firebaseio.com',
@@ -14,6 +27,8 @@ const firebaseConfig = {
   appId: '1:491470877076:web:830014bce1da5bcffcdf81',
   measurementId: 'G-QRKNDLEST4',
 };
+
+const firebaseConfig = APP_ENV === 'production' ? firebaseConfigProd : firebaseConfigDev;
 
 // Initialize Firebase
 export const initFirebase = initializeApp(firebaseConfig);
